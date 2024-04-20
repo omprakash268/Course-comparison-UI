e7b6fbdbfc85163032235db93f17d535b2c4b8ab
Om Prakash <omprakashmaurya268@gmail.com>
2024-04-20 14:20:33 +0530

    Navbar added

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,27 @@
+import logo from "../../assets/images/logo.png";
+export const Header = () => {
+  return (
+    <div className="w-screen min-h-[4rem] bg-slate-500 flex justify-between items-center p-1 box-border">
+      <div className="logo w-[30%] flex justify-start items-center ml-[6rem]">
+        <img src={logo} alt="" className="h-[4rem] cursor-pointer" />
+      </div>
+      <ul className="flex justify-start items-center px-2 w-[70%]">
+        <li className="flex justify-center items-center p-2 hover:text-white cursor-pointer">
+          Courses
+        </li>
+        <li className="flex justify-center items-center p-2 hover:text-white cursor-pointer">
+          Proiders
+        </li>
+        <li className="flex justify-center items-center p-2 hover:text-white cursor-pointer">
+          Explore
+        </li>
+        <li className="flex justify-center items-center p-2 hover:text-white cursor-pointer">
+          Contact Us
+        </li>
+        <li className="flex justify-center items-center p-2 hover:text-white cursor-pointer">
+          About Us
+        </li>
+      </ul>
+    </div>
+  );
+};
